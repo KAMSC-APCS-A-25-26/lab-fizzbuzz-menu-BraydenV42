c56b3bebb82b027cf0bcecda810af8186723e12c
BraydenV42 <brayriley44@gmail.com>
2025-09-22 13:36:29 -0400

    Update FizzBuzzMenu.java

--- src/main/java/FizzBuzzMenu.java
+++ src/main/java/FizzBuzzMenu.java
@@ -8,22 +8,38 @@ public class FizzBuzzMenu {
         
         while(running) {
             int choice;
-            System.out.println("------------Bank Account-------------");
-            System.out.println("Add money");
-            System.out.println("Withdraw money");
-            System.out.println("Check balance");
-            System.out.println("Exit");
+            System.out.println("------------FizzBuzz Menu-------------");
+            System.out.println("1: Display Fizz numbers (multiples of 3)");
+            System.out.println("2: Display Buzz numbers (multiples of 5)");
+            System.out.println("3: Display FizzBuzz numbers (multiples of both 3 and 5)");
+            System.out.println("4: Exit the program");
             System.out.println("--------------------------------------");
             System.out.println("Whats your choice?");
             choice = sc.nextInt();
+            switch(choice) {
                 case 1:
-                    bankAccount += sc.nextDouble;
+                    for(int i = 0; i < 100; i++)
+                    {
+                        if (i % 3 == 0) {
+                            System.out.println("All Fizz Values: " + i);
+                        }
+                    }
                     break;
                 case 2:
-                    bankAccount -= sc.nextDouble;
+                    for(int j = 0; j < 100; j++)
+                    {
+                        if (j % 5 == 0) {
+                            System.out.println("All Buzz Values: " + j);
+                        }
+                    }
                     break;
                 case 3: 
-                    System.out.println(bankAccount);
+                    for(int k = 0; k < 100; k++)
+                    {
+                        if (k % 5 == 0 && k % 3 == 0) {
+                            System.out.println("All FizzBuzz Values: " + k);
+                        }
+                    }
                     break;
                 case 4:
                     running = false;

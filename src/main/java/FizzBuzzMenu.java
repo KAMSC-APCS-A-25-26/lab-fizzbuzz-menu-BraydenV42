2476bda657f7ffd5a565372ab8967b8d7d535e69
BraydenV42 <brayriley44@gmail.com>
2025-09-22 13:40:26 -0400

    Update FizzBuzzMenu.java

--- src/main/java/FizzBuzzMenu.java
+++ src/main/java/FizzBuzzMenu.java
@@ -3,43 +3,30 @@ import java.util.Scanner;
 public class FizzBuzzMenu {
     public static void main(String[] args) {
         boolean running = true;
-        
+        double bankAccount = 0.0;
         Scanner sc = new Scanner(System.in);
         
         while(running) {
             int choice;
-            System.out.println("------------FizzBuzz Menu-------------");
-            System.out.println("1: Display Fizz numbers (multiples of 3)");
-            System.out.println("2: Display Buzz numbers (multiples of 5)");
-            System.out.println("3: Display FizzBuzz numbers (multiples of both 3 and 5)");
-            System.out.println("4: Exit the program");
+            System.out.println("------------Bank Account-------------");
+            System.out.println("Add money");
+            System.out.println("Withdraw money");
+            System.out.println("Check balance");
+            System.out.println("Exit");
             System.out.println("--------------------------------------");
             System.out.println("Whats your choice?");
             choice = sc.nextInt();
             switch(choice) {
                 case 1:
-                    for(int i = 0; i < 100; i++)
-                    {
-                        if (i % 3 == 0) {
-                            System.out.println("All Fizz Values: " + i);
-                        }
-                    }
+                    double add = sc.nextDouble();
+                    bankAccount += add;
                     break;
                 case 2:
-                    for(int j = 0; j < 100; j++)
-                    {
-                        if (j % 5 == 0) {
-                            System.out.println("All Buzz Values: " + j);
-                        }
-                    }
+                    double subtract = sc.nextDouble();
+                    bankAccount -= subtract;
                     break;
                 case 3: 
-                    for(int k = 0; k < 100; k++)
-                    {
-                        if (k % 5 == 0 && k % 3 == 0) {
-                            System.out.println("All FizzBuzz Values: " + k);
-                        }
-                    }
+                    System.out.println(bankAccount);
                     break;
                 case 4:
                     running = false;
